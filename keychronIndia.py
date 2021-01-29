a311c19704ccea3cb42b2b4495c61c8108396b89
Upendra <eupendra@gmail.com>
2021-01-29 13:30:07 +0530

    refactor

--- keychronIndia.py
+++ keychronIndia.py
@@ -2,44 +2,60 @@ import scrapy
 import json
 import os
 from scrapy.crawler import CrawlerProcess
-CSV_FILE = 'k4.csv'
+import pandas as pd
+
+CSV_FILE = 'k4India.csv'
+# Set Debug to True to get mails even if No stock
+DEBUG = False
 
 
-class KeychronSpider(scrapy.Spider):
+class KeychronIndiaSpider(scrapy.Spider):
     custom_settings = {
         'FEEDS': {
             CSV_FILE: {
                 'format': 'csv',
-                'encoding': 'utf-8'
+                'encoding': 'utf-8',
+                'overwrite': True
             },
         },
-        'LOG_LEVEL' : 'WARNING'
+        'LOG_LEVEL': 'DEBUG' if DEBUG else 'WARN'
     }
-    name = 'keychron'
+    name = 'keychron_india'
     start_urls = [
         'https://keychron.in/product/keychron-k1-wireless-mechanical-keyboard-version-3',
         'https://keychron.in/product/keychron-k2-wireless-mechanical-keyboard',
         'https://keychron.in/product/keychron-k4-wireless-mechanical-keyboard',
-        'https://keychron.in/product/keychron-k6-wireless-mechanical-keyboard',
+        'https://keychron.in/product/keychron-k6-wireless-mechanical-keyboard'
     ]
 
     def parse(self, response):
+        # inspect_response(response, self)
         raw_data = response.xpath(
-            '//form[@class="variations_form cart"]/@data-product_variations').get()
+            '//form[@class="variations_form cart"]/@data-product_variations'
+        ).get()
         data = json.loads(raw_data)
         for item in data:
-            version = item["attributes"].get("attribute_pa_version","")
-            switch_option = item["attributes"].get("attribute_pa_switch-option","") 
-            if not version and not switch_option:
-                version = item["attributes"].get("attribute_pa_key")
-                
+            version = item["attributes"].get("attribute_pa_version", "")
+            switch_option = item["attributes"].get("attribute_pa_switch-option", "")
+            keys = item["attributes"].get("attribute_pa_key", "")
+            available = item['is_in_stock']
             yield {
-                'name': version + switch_option,
-                'type': response.url.split('/')[-1],
-                'stock': item["is_in_stock"],
+                'name': version + switch_option + keys,
+                'available': available,
                 'display_regular_price': item['display_regular_price'],
                 'url': response.url
             }
+
+
+def get_body_subject():
+    df = pd.read_csv(CSV_FILE)
+    if df["available"].any():
+        subject = "✔ Keychron(IN) - Available"
+        body = df[df["available"]].to_html()
+    else:
+        subject = "❌ Keychron(IN) - Out of Stock" if DEBUG else None
+        body = df.to_html() if DEBUG else None
+    return body, subject
 
 
 def send_mail():
@@ -47,36 +63,41 @@ def send_mail():
     from email.message import EmailMessage
     try:
         msg = EmailMessage()
-        EMAIL_USER = os.environ.get('EMAIL_USER')
-        EMAIL_PASS = os.environ.get('EMAIL_PWD')
-        msg['From'] = EMAIL_USER
-        msg['To'] = EMAIL_USER
-        
-        import pandas as pd
+        MAIL_USER = os.environ.get('ZMAIL_FROM_USER')
+        MAIL_PASS = os.environ.get('ZMAIL_PWD')
+        MAIL_TO = os.environ.get('ZMAIL_TO_USER')
+        body, subject = get_body_subject()
+        if not (body and subject) and not DEBUG:
+            print("Nothing in stock, exiting...")
+            return
+        msg["Subject"] = subject
+        msg['From'] = MAIL_USER
+        msg['To'] = MAIL_TO
+        msg['Cc'] = MAIL_USER
 
-        df = pd.read_csv(CSV_FILE)
-        msg.set_content(df.to_html(), subtype='html')
-        if df["stock"].any():
-            msg["Subject"] = "✔ Keychron IN - In STOCK"
-            msg.set_content(df[df["stock"]].to_html(), subtype='html')
-            with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-                smtp.starttls()
-                smtp.login(EMAIL_USER, EMAIL_PASS)
-                smtp.send_message(msg)
+        msg.set_content(body, subtype='html')
 
-    except:
-        print("Error in Sending Mail")
+        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+            smtp.starttls()
+            smtp.login(MAIL_USER, MAIL_PASS)
+            smtp.send_message(msg)
 
-    print("Mail Sent!")
-
-
-def cleanup():
-    if os.path.exists(CSV_FILE):
-        os.remove(CSV_FILE)
+    except Exception as e:
+        print("Error in Sending Mail\n", str(e))
+    else:
+        print("Mail Sent!")
 
 
-cleanup()
-process = CrawlerProcess()
-process.crawl(KeychronSpider)
-process.start()
-send_mail()
+def main():
+    try:
+        process = CrawlerProcess()
+        process.crawl(KeychronIndiaSpider)
+        process.start()
+    except Exception as e:
+        print('Unexpected error\n' + str(e))
+    else:
+        send_mail()
+
+
+if __name__ == '__main__':
+    main()
--- keychronUS.py
+++ keychronUS.py
@@ -2,61 +2,100 @@ import scrapy
 import json
 import os
 from scrapy.crawler import CrawlerProcess
+import pandas as pd
 
-CSV_FILE = 'k4.csv'
+CSV_FILE = 'k4US.csv'
+# Set Debug to True to get mails even if No stock
+DEBUG = False
+
+
 class KeychronSpider(scrapy.Spider):
     custom_settings = {
         'FEEDS': {
             CSV_FILE: {
                 'format': 'csv',
-                'encoding': 'utf-8'
+                'encoding': 'utf-8',
+                'overwrite': True
             },
-        }
+        },
+        'LOG_LEVEL': 'DEBUG' if DEBUG else 'WARN'
     }
-    name='keychron'
+    name = 'keychron'
     start_urls = [
-        'https://www.keychron.com/products/keychron-k4-wireless-mechanical-keyboard-version-2']
+        'https://www.keychron.com/products/keychron-k4-wireless-mechanical-keyboard-version-2',
+        'https://www.keychron.com/products/keychron-k2-hot-swappable-wireless-mechanical-keyboard',
+        'https://www.keychron.com/products/keychron-k2-wireless-mechanical-keyboard',
+        'https://www.keychron.com/products/keychron-k8-tenkeyless-wireless-mechanical-keyboard',
+
+    ]
 
     def parse(self, response):
 
         data = json.loads(response.xpath(
-            '//script[@type="application/ld+json"]/text()')[0].get())
+            '//script[@type="application/ld+json"]/text()').get()
+        )
+
         for var in data['offers']:
+            available = True if 'InStock' in var['availability'] else False
             yield {
                 'name': var['name'],
-                'availability': var['availability'].strip('http://schema.org/'),
-                'price': var['price']
+                'available': available,
+                'price': var['price'],
+                'url': response.url
             }
+
+
+def get_body_subject():
+    df = pd.read_csv(CSV_FILE)
+    if df["available"].any():
+        subject = "✔ Keychron(US) - Available"
+        body = df[df["available"]].to_html()
+    else:
+        subject = "❌ Keychron(US) - Out of Stock" if DEBUG else None
+        body = df.to_html() if DEBUG else None
+    return body, subject
 
 
 def send_mail():
     import smtplib
     from email.message import EmailMessage
+    try:
+        msg = EmailMessage()
+        MAIL_USER = os.environ.get('ZMAIL_FROM_USER')
+        MAIL_PASS = os.environ.get('ZMAIL_PWD')
+        MAIL_TO = os.environ.get('ZMAIL_TO_USER')
+        body, subject = get_body_subject()
+        if not (body and subject) and not DEBUG:
+            print("Nothing in stock, exiting...")
+            return
+        msg["Subject"] = subject
+        msg['From'] = MAIL_USER
+        msg['To'] = MAIL_TO
+        msg['Cc'] = MAIL_USER
 
-    msg = EmailMessage()
-    EMAIL_USER = os.environ.get('EMAIL_USER')
-    EMAIL_PASS = os.environ.get('EMAIL_PWD')
-    msg['From'] = EMAIL_USER
-    msg['To'] = EMAIL_USER
-    msg["Subject"] = "Keychron Availability Status"
-    # Message body
-    import pandas as pd         
-    df = pd.read_csv(CSV_FILE) 
-    msg.set_content(df.to_html(), subtype='html')
-    
-    with smtplib.SMTP("smtp.gmail.com",587) as smtp:
-        smtp.starttls()
-        smtp.login(EMAIL_USER, EMAIL_PASS)
-        smtp.send_message(msg)
+        msg.set_content(body, subtype='html')
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+            smtp.starttls()
+            smtp.login(MAIL_USER, MAIL_PASS)
+            smtp.send_message(msg)
+
+    except Exception as e:
+        print("Error in Sending Mail\n", str(e))
+    else:
+        print("Mail Sent!")
 
 
+def main():
+    try:
+        process = CrawlerProcess()
+        process.crawl(KeychronSpider)
+        process.start()
+    except Exception as e:
+        print('Unexpected error\n' + str(e))
+    else:
+        send_mail()
 
-def cleanup():
-    if os.path.exists(CSV_FILE):
-        os.remove(CSV_FILE)
-    
-cleanup()
-process = CrawlerProcess()
-process.crawl(KeychronSpider)
-process.start()
-send_mail()
+
+if __name__ == '__main__':
+    main()
